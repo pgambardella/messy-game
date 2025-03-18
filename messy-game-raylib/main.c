726b5703864e92cde3f0631654427fdc4171ccc1
Paolo Gambardella <pgambardelladc@gmail.com>
2025-03-18 10:28:12 +0100

    Window 480x320

---
+++ messy-game-raylib/main.c
@@ -0,0 +1,36 @@
+// main.c : Messy Game
+// a Zelda clone where you are a mage that controls an enchanted ball like a PRO
+#include "raylib.h"
+#include "raymath.h"
+
+void GameStartup() {}
+void GameUpdate() {}
+void GameRender() {}
+void GameShutdown() {}
+
+const int screenWidth = 480;
+const int screenHeight = 320;
+
+int main()
+{
+    InitWindow(screenWidth, screenHeight, "Raylib Messy Game");
+    SetTargetFPS(60);
+
+    GameStartup();
+
+	while (!WindowShouldClose()) {
+		GameUpdate();
+		BeginDrawing();
+		ClearBackground(GRAY);
+
+		GameRender();
+
+		EndDrawing();
+
+	}
+
+	GameShutdown();
+
+	CloseWindow();
+	return 0;
+}
